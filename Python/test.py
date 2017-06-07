c9cd099fbd3fb591c511eeacf58f5e3e6572a6b8
MattK <matt@kmatt.org>
2017-06-07 12:42:49 -0400

    2to3

--- Python/test.py
+++ Python/test.py
@@ -2,25 +2,25 @@ import breakout_detection
 
 Z = []
 
-for i in xrange(64):
+for i in range(64):
     Z.append(0.0)
 
-for i in xrange(64):
+for i in range(64):
     Z.append(1.0)
 
 edm_per = breakout_detection.EdmPer()
 edm_per.evaluate(Z)
-print edm_per.getLoc()
+print(edm_per.getLoc())
 
 edm_multi = breakout_detection.EdmMulti()
 edm_multi.evaluate(Z)
-print edm_multi.getLoc()
+print(edm_multi.getLoc())
 
 edm_tail = breakout_detection.EdmTail()
 edm_tail.evaluate(Z)
-print edm_tail.getLoc()
+print(edm_tail.getLoc())
 
 edmx = breakout_detection.Edmx()
 edmx.evaluate(Z)
-print edmx.getLoc()
+print(edmx.getLoc())
 
